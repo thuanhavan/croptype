ab60c41e5f5ad20aa6c3965e662e8c8fbd02cf30
Ha <hvt632@usask.ca>
2023-03-17 17:59:44 -0600

    'setting'

--- create_dataset.py
+++ create_dataset.py
@@ -238,6 +238,89 @@ if __name__ == "__main__":
     else:
         raise ValueError(f"framework not supported: {args.framework}")
 
+
+
+#-----------------CLOUD-----------------------
+def get_s2(aoi, start_date, end_date):
+    # Import and filter S2
+    
+    CLOUD_FILTER = 20
+    CLD_PRB_THRESH = 5
+    NIR_DRK_THRESH = 0.15
+    CLD_PRJ_DIST = 1
+    BUFFER = 1
+
+    s2_sr_col = (ee.ImageCollection('COPERNICUS/S2_SR')
+                 .filterBounds(aoi)
+                 .filterDate(start_date, end_date)
+                 .filter(ee.Filter.lte('CLOUDY_PIXEL_PERCENTAGE', CLOUD_FILTER))
+                 .map(lambda im: im.clip(aoi)))
+
+    # Import and filter s2cloudless.
+    s2_cloudless_col = (ee.ImageCollection('COPERNICUS/S2_CLOUD_PROBABILITY')
+                        .filterBounds(aoi)
+                        .filterDate(start_date, end_date))
+
+    # Join the filtered s2cloudless collection to the SR collection by the 'system:index' property.
+    return ee.ImageCollection(ee.Join.saveFirst('s2cloudless').apply(primary = s2_sr_col, 
+                                                                      secondary = s2_cloudless_col, 
+                                                                      condition = ee.Filter.equals(leftField='system:index', rightField='system:index')))
+
+# Cloud probability threshold.
+CLD_PRB_THRESH = 50
+NIR_DRK_THRESH = 0.15
+CLD_PRJ_DIST = 2
+BUFFER = 8
+
+def cloud_mask(img):
+    
+    # Add cloud component bands.
+    def add_cloud_bands(img):
+        # Get s2cloudless image, subset the probability band.
+        cld_prb = ee.Image(img.get('s2cloudless')).select('probability')
+        # Condition s2cloudless by the probability threshold value.
+        is_cloud = cld_prb.gt(CLD_PRB_THRESH).rename('clouds')
+        # Add the cloud probability layer and cloud mask as image bands.
+        return img.addBands(ee.Image([cld_prb, is_cloud]))
+    
+    img_cloud = add_cloud_bands(img)
+   
+    # Add cloud shadow component bands.
+    def add_shadow_bands(img):
+        # Identify water pixels from the SCL band.
+        not_water = img.select('SCL').neq(6)
+        # Identify dark NIR pixels that are not water (potential cloud shadow pixels).
+        SR_BAND_SCALE = 1e4
+        dark_pixels = img.select('B8').lt(NIR_DRK_THRESH*SR_BAND_SCALE).multiply(not_water).rename('dark_pixels')
+        # Determine the direction to project cloud shadow from clouds (assumes UTM projection).
+        shadow_azimuth = ee.Number(90).subtract(ee.Number(img.get('MEAN_SOLAR_AZIMUTH_ANGLE')))
+        # Project shadows from clouds for the distance specified by the CLD_PRJ_DIST input.
+        cld_proj = (img.select('clouds').directionalDistanceTransform(shadow_azimuth, CLD_PRJ_DIST*10)
+            .reproject(crs=img.select(0).projection().crs(), scale=100)
+            .select('distance')
+            .mask()
+            .rename('cloud_transform'))
+        # Identify the intersection of dark pixels with cloud shadow projection.
+        shadows = cld_proj.multiply(dark_pixels).rename('shadows')
+        # Add dark pixels, cloud projection, and identified shadows as image bands.
+        return img.addBands(ee.Image([dark_pixels, cld_proj, shadows]))
+    
+    img_cloud_shadow = add_shadow_bands(img_cloud)
+
+    # Combine cloud and shadow mask, set cloud and shadow as value 1, else 0.
+    is_cld_shdw = img_cloud_shadow.select('clouds').add(img_cloud_shadow.select('shadows')).gt(0)
+
+    # Remove small cloud-shadow patches and dilate remaining pixels by BUFFER input.
+    # 20 m scale is for speed, and assumes clouds don't require 10 m precision.
+    is_cld_shdw2 = (is_cld_shdw.focal_min(2).focal_max(BUFFER*2/20)
+        .reproject(crs=img.select([0]).projection().crs(), scale=20)
+        .rename('cloudmask'))
+
+    # Add the final cloud-shadow mask to the image.
+    return img_cloud_shadow.addBands(is_cld_shdw2)
+    
+    
+  
 # function to mosaic all s2 data
 def mosaicByDate(imgCol):
   imlist = imgCol.toList(imgCol.size())
